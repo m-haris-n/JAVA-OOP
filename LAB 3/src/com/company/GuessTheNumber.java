73f99878d7f84b7475d7bef5ec87045a087ebf95
M-Haris-N <m.haris.nabeel@gmail.com>
2021-10-16 22:28:26 -0700

    updated guessNumber

--- LAB 3/src/com/company/GuessTheNumber.java
+++ LAB 3/src/com/company/GuessTheNumber.java
@@ -7,13 +7,17 @@ public class GuessTheNumber {
     private int warning = 4;
     private int guessedNo;
     private int[] alreadyTold = new int[10];
+    private int specialWarning = 1;
     int index = 0;
     int rng = 20;
 
-    public int inputInt(){
-        System.out.print("Enter Your Guess: ");
-        Scanner num = new Scanner(System.in);
-        return num.nextInt();
+    public Scanner inputInt(){
+            System.out.print("\nEnter Your Guess: ");
+            return new Scanner(System.in);
+    }
+
+    public boolean isValid(Scanner inp){
+        return inp.hasNextInt();
     }
 
     public int generateRand(int range){
@@ -21,7 +25,7 @@ public class GuessTheNumber {
         return rand.nextInt(range);
     }
     public void setAlreadyTold(int guess){
-        this.alreadyTold[index] = guess;
+        this.alreadyTold[this.index] = guess;
         this.index++;
     }
 
@@ -33,40 +37,63 @@ public class GuessTheNumber {
         }
         return false;
     }
+    public void printGuessed(){
+        for(int i = 0; i < 10; i ++){
+            if(alreadyTold[i] != -1)
+            System.out.print(alreadyTold[i] + " ");
+        }
+    }
+
+    public void setGuessedToNull(){
+        for(int i = 0; i < 10; i ++){
+            alreadyTold[i] = -1;
+        }
+    }
 
     public void play(){
+        setGuessedToNull();
         int secretNum = generateRand(rng);
         System.out.println("I am Thinking of a word between 0-"+ rng + ".");
+        Scanner scanGuess;
         int guess;
         boolean flag = true;
         do{
-            System.out.println("Remaining guesses: "+this.guesses+"\nRemaining warnings: "+this.warning);
-            guess = inputInt();
-            if(guess == secretNum)
-            {
-                System.out.println("Congratulations! You have guessed the secret number!!");
-                flag = false;
-            }
-            else if(this.warning==0 || this.guesses==0){
+            System.out.print("Remaining guesses: "+this.guesses+"\nRemaining warnings: "+this.warning + "\nGuessed Numbers: "); printGuessed();
+            scanGuess = inputInt();
+            if(isValid(scanGuess)){
+                guess = scanGuess.nextInt();
+                if(guess == secretNum)
+                {
+                    System.out.println("Congratulations! You have guessed the secret number!!");
+                    flag = false;
+                }
+                else if ((this.warning) > 0 && (this.guesses > 0)) {
+
+                        if(isAlreadyTold(guess)){
+                            this.warning--;
+                            this.guesses--;
+                            System.out.println("You have already guessed this number");
+                        }else
+                        if(guess < secretNum){
+                            this.guesses--;
+                            System.out.println("Guessed number is less than secret number.");
+                            setAlreadyTold(guess);
+                        }else {
+                            this.guesses--;
+                            System.out.println("Guessed number is greater than secret number.");
+                            setAlreadyTold(guess);
+                        }
+
+                } else {
                     System.out.println("Game Over!");
                     flag = false;
-            }
-            else
-            {
-                if(isAlreadyTold(guess)){
-                    this.warning--;
-                    this.guesses--;
-                    System.out.println("You have already guessed this number");
-                }else
-                if(guess < secretNum){
-                    this.guesses--;
-                    System.out.println("Guessed number is less than secret number.");
-                    setAlreadyTold(guess);
-                }else {
-                    this.guesses--;
-                    System.out.println("Guessed number is greater than secret number.");
-                    setAlreadyTold(guess);
                 }
+            } else if (this.specialWarning>0){
+                System.out.println("Invalid Input!!!");
+                this.specialWarning--;
+            }else{
+                System.out.println("Invalid Input!!!");
+                this.guesses--;
             }
 
         }while(flag);
